a079384ef04d9fb2827517a9d018c52eed342fcf
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 19:04:47 +0530

    added code,and comments to make the program more understandable for beginners

--- p2.c
+++ p2.c
@@ -1,17 +1,21 @@
+//this program can be used to find the GCD and LCM of any two whole numbers
 #include<stdio.h>
 int main()
 {
     int a,r,l,b;
-    printf("enter the two number\n");
-    scanf("%d%d",&a,&b);
+    printf("Enter the first number-\n");
+    scanf("%d",&a);//read the first number
+    printf("Enter the second number-\n");
+    scanf("%d",&b);//read the second number
     int x=a;
     int y=b;
-    while(b!=0)
+    while(b!=0)//while loop
     {
         r=a%b;
         a=b;
         b=r;
     }
     l=(x*y)/a;
-    printf("GCD=%d & LCM=%d\n",a,l);
+    printf("GCD=%d & LCM=%d\n",a,l);//print the values of GCD and LCM
+    return 0;
 }
