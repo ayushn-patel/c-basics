35d05fceb9893d54aff92c33b95e6b842011b095
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 18:39:03 +0530

    seventh program

---
+++ p7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	
+	int n;
+	printf("Enter the number");
+	int num;
+	scanf("%d",&num);
+	int sum=0,dig=0;
+	while(num>0)
+	{
+	    dig=num%10;
+	    sum+=dig;
+	    num/=10;
+	}
+	printf("The sum of digits of a number is: %d",sum);
+}
