4cfbaa28cd3fdff6bd17acd0cb1d92a03f45a30b
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 19:21:14 +0530

    added code,and comments to make the program more understandable for beginners

--- p6.c
+++ p6.c
@@ -1,23 +1,24 @@
+//this program can be used to convert binary nuimbers to decimal numbers
 #include <stdio.h>
 #include <math.h>
-int x(long long n);
+int convertBinaryToDecimal(long long n);
 int main()
 {
     long long n;
     printf("Enter a binary number: ");
     scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, x(n));
+    printf("%lld in binary = %d in decimal", n, convertBinaryToDecimal(n));
     return 0;
 }
-int x(long long n)
+int convertBinaryToDecimal(long long n)
 {
-    int y = 0, i = 0, remainder;
+    int decimalnumber = 0, i = 0, remainder;
     while (n!=0)
     {
         remainder = n%10;
         n /= 10;
-        y += remainder*pow(2,i);
+        decimalnumber += remainder*pow(2,i);
         ++i;
     }
-    return y;
+    return decimalnumber;
 }
