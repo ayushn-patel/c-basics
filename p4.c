1139fd25390b2e4db48dacff0554c4d2480a5c43
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 18:26:31 +0530

    fourth program

---
+++ p4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fact(int n)
+{
+int f;
+
+    if( n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        f=n*fact(n-1);
+    }
+    return f;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number \n");
+    scanf("%d",&n);
+    printf("Factorial of a number =% d \n",fact(n));
+    return 0;
+}
