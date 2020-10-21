2be67b4d1fc33695823f6bdcfdb0ddf89700e8e6
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 19:13:31 +0530

    added code,and comments to make the program more understandable for beginners

--- p4.c
+++ p4.c
@@ -1,3 +1,5 @@
+//this program can be used to find the factorial of a number
+//this program is also an introduction to functions in C
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -20,7 +22,7 @@ int main()
 {
     int n;
     printf("Enter the number \n");
-    scanf("%d",&n);
-    printf("Factorial of a number =% d \n",fact(n));
+    scanf("%d",&n);//read the number
+    printf("Factorial of a number =% d \n",fact(n));//calling function fact
     return 0;
 }
