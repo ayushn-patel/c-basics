f4bc9720988676a6d33254e9605387b601681451
Ayush N Patel <reachayushnpatel@gmail.com>
2020-10-21 19:08:41 +0530

    added code,and comments to make the program more understandable for beginners

--- p3.c
+++ p3.c
@@ -1,13 +1,15 @@
+//this program can be used to find the number of digits in a number
 #include<stdio.h>
 int main()
 {
    int n,count=0;
-    printf("Enter the number\n");
-    scanf("%d",&n);
-    while(n!=0)
+    printf("Enter the number-\n");
+    scanf("%d",&n);//read the number
+    while(n!=0)//while loop
     {
         count++;
-        n/=10;
+        n=n/10;
     }
-printf("number of digits =%d\n",count);
+printf("number of digits =%d\n",count);//print the number of digits calculated in the while loop
+return 0;
 }
